2facd2ad530c44d2ec6ffa5bbaddae9d91aec723
cahuerta <cahuerta@gmail.com>
2026-01-13 21:55:38 -0300

    Update universe.js

--- js/universe.js
+++ js/universe.js
@@ -89,7 +89,8 @@ export async function loadUniverse(force = false) {
         apiGet(`/dashboard/latest/${t}`)
           .then(r => ({
             ticker: t,
-            data: r?.latest?.prediction || null
+            data: r?.latest?.result || null
+
           }))
       )
     );
