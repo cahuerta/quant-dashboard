29e34afbaa0eea8453a56d770c5b504588384d59
cahuerta <cahuerta@gmail.com>
2026-02-15 18:35:31 -0300

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./Layout";
 
 import Global from "../pages/Global";
 import Universe from "../pages/Universe";
+import UniverseChile from "../pages/UniverseChile";
 import Signals from "../pages/Signals";
 import Screener from "../pages/Screener";
 import Portfolio from "../pages/Portfolio";
@@ -13,7 +14,7 @@ export default function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Global />} />
         <Route path="/universe" element={<Universe />} />
-        <Route path="/universe-cl" element={<Universe />} />
+        <Route path="/universe-cl" element={<UniverseChile />} />
         <Route path="/signals" element={<Signals />} />
         <Route path="/screener" element={<Screener />} />
         <Route path="/portfolio" element={<Portfolio />} />
