6506ca177d606786596058dff9ac268038c56131
cahuerta <cahuerta@gmail.com>
2026-02-15 14:02:37 -0300

    Delete js directory

--- js/api.js
+++
@@ -1,9 +0,0 @@
-// js/api.js
-
-const API = "https://spy-2w-price-prediction.onrender.com";
-
-export async function apiGet(url) {
-  const r = await fetch(API + url);
-  if (!r.ok) throw new Error(`HTTP ${r.status}`);
-  return await r.json();
-}
